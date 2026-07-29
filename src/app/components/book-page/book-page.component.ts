8e4fbc6f89a2bcb257af3bb8ea9983d31d562fd5
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2026-07-29 03:26:55 +0300

    [Cleanup] remove redundant code

--- src/app/components/book-page/book-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-book-page',
-  imports: [],
-  templateUrl: './book-page.component.html',
-  styleUrl: './book-page.component.css',
-})
-export class BookPageComponent {}
